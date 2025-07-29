c56f13406251fde67e1a72a4ca9afa17c04e8ffe
Fayzullojon <fayzullokhonazarov4@gmail.com>
2025-07-29 12:48:51 +0300

    dd

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -237,14 +237,14 @@ const Home = () => {
           <h1 className='text-[35px] text-center mb-[30px] font-bold'>Our benefits</h1>
           <div className='flex flex-wrap justify-center items-center gap-[20px] lg:justify-between'>
             <div className='flex flex-col gap-[20px]'>
-              <article className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
+              <article data-aos='zoom-in' className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-[#7772F1]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
                 </svg>
                 <h1 className='font-bold'>Social Advertising Experts</h1>
                 <p className='text-[#787A80] text-[12px]'>Culpa nostrud commodo ea consequat aliquip reprehenderit. Veniam velit nostrud aliquip sunt.</p>
               </article>
-              <article className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
+              <article data-aos='zoom-in' className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-[#7772F1]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
                 </svg>
@@ -252,7 +252,7 @@ const Home = () => {
                 <h1 className='font-bold'>Social Advertising Experts</h1>
                 <p className='text-[#787A80] text-[12px]'>Culpa nostrud commodo ea consequat aliquip reprehenderit. Veniam velit nostrud aliquip sunt.</p>
               </article>
-              <article className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
+              <article data-aos='zoom-in' className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-[#7772F1]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
                 </svg>
@@ -263,14 +263,14 @@ const Home = () => {
             </div>
             <img src={HeroN5} className='w-[300px] h-[400px]' alt="" />
             <div className='flex flex-col gap-[20px]'>
-              <article className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
+              <article data-aos='zoom-in' className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-[#7772F1]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
                 </svg>
                 <h1 className='font-bold'>Social Advertising Experts</h1>
                 <p className='text-[#787A80] text-[12px]'>Culpa nostrud commodo ea consequat aliquip reprehenderit. Veniam velit nostrud aliquip sunt.</p>
               </article>
-              <article className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
+              <article data-aos='zoom-in' className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-[#7772F1]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
                 </svg>
@@ -278,7 +278,7 @@ const Home = () => {
                 <h1 className='font-bold'>Social Advertising Experts</h1>
                 <p className='text-[#787A80] text-[12px]'>Culpa nostrud commodo ea consequat aliquip reprehenderit. Veniam velit nostrud aliquip sunt.</p>
               </article>
-              <article className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
+              <article data-aos='zoom-in' className='text-center px-[40px] py-[40px] items-center flex flex-col justify-between w-[300px] hover:shadow-lg dark:shadow-[#ffffff38] shadow-gray-600  h-[200px] rounded-[4px] border-[1px] dark:border-gray-700 border-[#E5E8ED]'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-[#7772F1]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
                 </svg>
@@ -290,6 +290,7 @@ const Home = () => {
         </section>
         <section className='px-[20px] flex lg:hidden py-[50px]'>
           <Swiper
+          data-aos='zoom-in'
             effect={'cards'}
             grabCursor={true}
             modules={[EffectCards]}
@@ -305,9 +306,15 @@ const Home = () => {
           </Swiper>
         </section>
         <section className='hidden lg:flex justify-between px-[150px] py-[50px]'>
-          <PricingCard plan={"Basic"} price={"24"} />
-          <PricingCard plan={"Basic"} price={"24"} />
-          <PricingCard plan={"Basic"} price={"24"} />
+         <div data-aos='zoom-in'>
+           <PricingCard  plan={"Basic"} price={"24"} />
+         </div>
+         <div data-aos='zoom-in'>
+           <PricingCard plan={"Basic"} price={"24"} />
+         </div>
+          <div data-aos='zoom-in'>
+           <PricingCard plan={"Basic"} price={"24"} />
+         </div>
         </section>
       </section>
 
